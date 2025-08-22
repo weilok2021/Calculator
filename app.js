020e243ea1b7b3a08955abffa098fbe0435ba555
weilok <weilok629@gmail.com>
2025-08-22 22:50:18 +0800

    nested calculation implemented now, but this is not work for different operators.

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ const calculator = document.querySelector('.calculator')
 function calculate(firstValue, operator, secondValue) {
     const num1 = parseFloat(firstValue);
     const num2 = parseFloat(secondValue);
-    console.log(`num1 = ${num1} and num2 = ${num2}`);
+    console.log(`num1 = ${num1} ,action: ${operator}, num2 = ${num2}`);
 
     if (operator === "add") {
         return num1 + num2;
     }
     else if (operator === "subtract") {
         return num1 - num2;
-    } 
+    }
     else if (operator === "multiply") {
         return num1 * num2;
     }
@@ -27,6 +27,7 @@ keys.addEventListener("click", (e) => {
         const key = e.target;
         const action = key.dataset.action;
         const previousKeyType = calculator.dataset.previousKeyType;
+        const previousAction = calculator.dataset.previousAction;
 
         // if action is undefined, then this is a number
         if (!action) {
@@ -44,46 +45,53 @@ keys.addEventListener("click", (e) => {
         }
         // else, this is an action
         else {
-            if (action === "add" || 
+            if (action === "add" ||
                 action === "subtract" ||
                 action === "multiply" ||
                 action === "divide"
             ) {
-                // save the current display value as dataset.firstValue
-                key.dataset.firstValue = display.innerText;
+                if (calculator.dataset.firstValue && calculator.dataset.firstValue !== "undefined") {
+                    // for example: 5 + 7 * 
+                    // in this case, user did not press equal and try to nest calculation
+                    // after I refactor this code, 5 is the calculator.dataset.firstValue, and 7 is the display.innerText
+                    // "add" is the calculator.dataset.previousAction, so we can calculate this expression and set this to be new firstValue
+                    calculator.dataset.firstValue = calculate(calculator.dataset.firstValue, previousAction, display.innerText);
+                    console.log(`firstValue: ${calculator.dataset.firstValue}, previousAction: ${previousAction}, display.innerText: ${display.innerText}`);
+                }
+                else {
+                    calculator.dataset.firstValue = display.innerText;
+                }
+
                 key.classList.add("is-depressed");
                 // to let the numKey know what's the previous keyType
                 calculator.dataset.previousKeyType = "operator";
+                // let the calculator know what's the previous action
+                calculator.dataset.previousAction = action;
             }
             else if (action === "decimal") {
                 if (previousKeyType === "number") {
                     display.innerText += ".";
                     calculator.dataset.previousKeyType = "decimal";
+                    // calculator.dataset.previousAction = action;
                 }
             }
             else if (action === "calculate") {
-                const operators = document.querySelectorAll(".key--operator");
-                let answer;
-                for (let operator of operators) {
-                    // this is the operator just being pressed by user
-                    // if first value exists within this operator, this is a valid calculation
-                    if (operator.dataset.firstValue && operator.dataset.firstValue !== "undefined") {
-                        console.log(`firstValue of ${operator.dataset.action} is ${operator.dataset.firstValue}`);
-                        answer = calculate(operator.dataset.firstValue, operator.dataset.action, display.innerText);
-                        // update the firstValue of operator to be undefined
-                        operator.dataset.firstValue = undefined;
-                        // update this calculated answer to the screen;
-                        display.innerText = answer;
-                        console.log(answer);
-                    }
+                if (calculator.dataset.firstValue && calculator.dataset.firstValue !== "undefined") {
+                    let answer = calculate(calculator.dataset.firstValue, calculator.dataset.previousAction, display.innerText);
+                    // update the firstValue of operator to be undefined
+                    calculator.dataset.firstValue = undefined;
+                    // update this calculated answer to the screen;
+                    display.innerText = answer;
+                    console.log(answer);
                 }
             }
             else if (action === "clear") {
                 // reset calculator state
                 calculator.dataset.previousKeyType = null;
+                calculator.dataset.previousAction = undefined;
+                calculator.dataset.firstValue = undefined;
                 display.innerText = 0;
             }
         }
-        // console.log(e.target.dataset.action);
     }
 });
